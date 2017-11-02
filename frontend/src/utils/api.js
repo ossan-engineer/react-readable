1a2d9eec48e68e779a2d1ab5dcd944e9cfa8204f
k.tachibana <bodega1123@gmail.com>
2017-11-03 04:33:58 +0900

    Export apiClient

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiClient = axios.create({
+export const apiClient = axios.create({
   baseURL: 'http://localhost:3001/',
   headers: {
     Authorization: 'whatever-you-want',
